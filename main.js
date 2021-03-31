2da22d9eb3817160f7e22247fa3da9926cf43620
Bartlomiej Parowicz <bartek@finibit.com>
2021-03-31 12:54:00 +0200

    EAdd main.js file

---
+++ main.js
@@ -0,0 +1,14 @@
+const config = require('config')
+
+const logger = require('./src/utils/logger')
+const app = require('./src/app')
+
+const port = process.env.PORT || config.get('feedmail.port')
+
+logger.info({ label: 'APP', message: ` ___ ___ ___ ___  __  __   _   ___ _     ` })
+logger.info({ label: 'APP', message: `| __| __| __|   \\|  \\/  | /_\\ |_ _| |    ` })
+logger.info({ label: 'APP', message: `| _|| _|| _|| |) | |\\/| |/ _ \\ | || |__  ` })
+logger.info({ label: 'APP', message: `|_| |___|___|___/|_|  |_/_/ \\_\\___|____| ` })
+logger.info({ label: 'APP', message: `                                         ` })
+
+app.listen(port)
